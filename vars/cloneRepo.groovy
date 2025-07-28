6d758326ad3d5939425a347e8d6040fe08d47126
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-07-28 16:41:20 -0500

    angulular builder creado

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -4,26 +4,21 @@ def call(Map config) {
         error("❌ cloneRepo: 'branch', 'repoPath', and 'repoUrl' parameters are required.")
     }
 
+    echo "📦 Cloning repository:"
+    echo "   🟢 Branch: ${config.branch}"
+    echo "   📁 Path: ${config.repoPath}"
+    echo "   🔗 URL: ${config.repoUrl}"
 
-    stage('Clone Repository') {
-        steps {
-            echo "📦 Cloning repository:"
-            echo "   🟢 Branch: ${config.branch}"
-            echo "   📁 Path: ${config.repoPath}"
-            echo "   🔗 URL: ${config.repoUrl}"
-
-            dir(config.repoPath) {
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: "*/${config.branch}"]],
-                    userRemoteConfigs: [[
-                        url: config.repoUrl,
-                        credentialsId: 'GITHUB'
-                    ]]
-                ])
-            }
-
-            echo "✅ Repository successfully cloned at: ${config.repoPath}"
-        }
+    dir(config.repoPath) {
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${config.branch}"]],
+            userRemoteConfigs: [[
+                url: config.repoUrl,
+                credentialsId: 'GITHUB'
+            ]]
+        ])
     }
+
+    echo "✅ Repository successfully cloned at: ${config.repoPath}"
 }
