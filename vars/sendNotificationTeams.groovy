36f30c7e503ba2f4b82ddbf1d1bcde962f4838ae
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-08-13 10:23:44 -0500

    [fix]: send notification

--- vars/sendNotificationTeams.groovy
+++ vars/sendNotificationTeams.groovy
@@ -1,54 +1,31 @@
-// File name: sendSuccessNotification.groovy
-
-
-    // // Definir dinámicamente el color y el emoji según el resultado
-    //             def status = currentBuild.currentResult
-    //             def color = "FF0000" // rojo por defecto
-    //             def emoji = "❌"
-    //             def statusText = "Build Failed"
-
-    //             if (status == "SUCCESS") {
-    //                 color = "00FF00" // verde
-    //                 emoji = "✅"
-    //                 statusText = "Build Succeeded"
-    //             } else if (status == "UNSTABLE") {
-    //                 color = "FFFF00" // amarillo
-    //                 emoji = "⚠️"
-    //                 statusText = "Build Unstable"
-    //             }
-
 def call (Map config) {
 
-  echo "📢 Enviando notificación de éxito a Microsoft Teams ${currentBuild.duration}"
+    echo "📢 Enviando notificación de éxito a Microsoft Teams"
+
     def durationMillis = currentBuild.duration
-    def seconds = (durationMillis / 1000) % 60
-    def minutes = (durationMillis / 1000 / 60) % 60
-    def hours = durationMillis / 1000 / 60 / 60
+    def totalSeconds = (durationMillis / 1000).toInteger()
+    def seconds = totalSeconds % 60
+    def minutes = (totalSeconds / 60) % 60
+    def hours = (totalSeconds / 3600)
 
     def durationText = ""
-    if (hours > 0) {
-    durationText += "${hours.toInteger()}h "
-    }
-    if (minutes > 0) {
-    durationText += "${minutes.toInteger()}m "
-    }
-    durationText += "${seconds.toInteger()}s"
-    
+    if (hours > 0) { durationText += "${hours}h " }
+    if (minutes > 0) { durationText += "${minutes}m " }
+    durationText += "${seconds}s"
+
     office365ConnectorSend(
         status: currentBuild.currentResult,
         message: "❌ Build Failed: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
         adaptiveCards: true,
-        color:"FF0000",
+        color: "FF0000",
         factDefinitions: [
             [name: "Build triggered by", template: env.BUILD_USER],
-            [name: "Commit Author", template:env.COMMIT_AUTHOR],
+            [name: "Commit Author", template: env.COMMIT_AUTHOR],
             [name: "Commit Message", template: env.COMMIT_MESSAGE],
             [name: "Commit Hash", template: env.COMMIT_HASH],
             [name: "Build", template: env.BUILD_NUMBER],
-            [name: "Remarks", template: env.currentBuild],
-            [name: "DeployTime", template: durationText],
+            [name: "Remarks", template: currentBuild.fullDisplayName],
+            [name: "DeployTime", template: durationText]
         ]
-
-
     )
 }
