ee60fbf131be25ba920cac0bd946391a61edd468
Edward-Arroyave <edward.arroyave@ithealth.co>
2025-08-21 10:00:14 -0500

    [add]: se agrega ambiente  mensaje de confirmacion

--- vars/sendNotificationTeams.groovy
+++ vars/sendNotificationTeams.groovy
@@ -16,18 +16,19 @@ def call(Map config) {
 
     // Determinar color y emoji según resultado
     def status = currentBuild.currentResult ?: "FAILURE"
-    // Mapa de estados base
-    def statusMap = [
-        "SUCCESS" : [color: "00FF00", emoji: "✅", statusText: "Build Succeeded"],
-        "UNSTABLE": [color: "FFFF00", emoji: "⚠️", statusText: "Build Unstable"],
-        "ABORTED" : [color: "FFA500", emoji: "⏹️", statusText: "Build Aborted"],
-        "FAILURE" : [color: "FF0000", emoji: "❌", statusText: "Build Failed"]
-    ]
+    def color = "FF0000"
+    def emoji = "❌"
+    def statusText = "Build Failed"
 
-    // Valores por defecto según el estado actual
-    def (color, emoji, statusText) = statusMap[status]?.values() ?: statusMap["FAILURE"].values()
-
-   
+    if (status == "SUCCESS") {
+        color = "00FF00"
+        emoji = "✅"
+        statusText = "Build Succeeded"
+    } else if (status == "UNSTABLE") {
+        color = "FFFF00"
+        emoji = "⚠️"
+        statusText = "Build Unstable"
+    }
 
     // Enviar notificación a Teams
     wrap([$class: 'BuildUser']) {
